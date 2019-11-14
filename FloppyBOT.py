4540fd249ad066e55a352c66a1c5e2e17d5abf5a
FloppyCat99 <57762701+FloppyCat99@users.noreply.github.com>
2019-11-14 17:10:19 +0200

    FloppyBOT
    Main python file

---
+++ FloppyBOT.py
@@ -0,0 +1,16 @@
+import discord
+import asyncio 
+
+from discord import commands.ext
+
+bot = commands.Bot(command_prefix='?')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+bot.run('token')
