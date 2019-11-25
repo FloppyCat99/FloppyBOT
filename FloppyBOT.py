6b903dbbd9b54d094755ff42bc25d5bfefd49c60
ToddyHowdy <57714056+ToddyHowdy@users.noreply.github.com>
2019-11-25 03:49:57 +0100

    Update FloppyBOT.py

--- FloppyBOT.py
+++ FloppyBOT.py
@@ -1,25 +1,2 @@
-#Import Stuff
-import discord
-import asyncio 
-
-from discord.ext import commands
-
-bot = commands.Bot(command_prefix="fp!")
-
-#Remove default help command
-bot.remove_command("help")
-
-#Show that the bot is online
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="fp!help"))
-
-@bot.command()
-async def help(ctx):
-    await ctx.send("The bot curently has no commands since floppy can't decide what he wants the bot to do")
-
-bot.run('token')
+404
+bot not found
